3d74b45a9991fe3541fd7ca15c32770020b17caa
Reggie Bowers <bowersrd@goldmail.etsu.edu>
2020-07-20 15:27:40 -0400

    Add InfoSection Component

---
+++ src/components/InfoSection.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="info-wrapper" :style="reverse ? 'flexDirection: row-reverse' : ''">
+    <div class="info" :style="reverse ? 'textAlign: right' : ''">
+      <h1 class="info__title">
+        {{ title }}
+        <span class="info__span">{{ span }}</span>
+      </h1>
+      <p class="info__text" :style="reverse ? 'paddingLeft: 5rem' : 'paddingRight: 5rem'">{{ text }}</p>
+      <a href="#" class="info__btn" v-if="btnTwo"> {{ btnOne }} </a>
+      <a href="#" class="info__btn info__btn--filled" v-if="btnTwo">{{ btnTwo }}</a>
+    </div>
+    <img src="../assets/images/travel_insurance.jpg" alt="Vacation Photo" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    span: String,
+    text: String,
+    btnOne: String,
+    btnTwo: String,
+    reverse: Boolean,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.info-wrapper {
+  width: 100%;
+  max-width: 1120px;
+  display: flex;
+  align-items: center;
+  padding: 12rem 0;
+}
+
+.info {
+  width: 60%;
+  line-height: 1.5;
+  &__title {
+    font-size: 4.5rem;
+    font-weight: 600;
+    margin-bottom: 3rem;
+  }
+  &__text {
+    font-size: 1.6rem;
+    font-weight: 400;
+    margin-bottom: 6rem;
+  }
+  &__btn {
+    border: 1px solid #ff7700;
+    padding: 1.8rem 4.3rem;
+    font-size: 1.6rem;
+    border-radius: 0.75rem;
+    font-weight: 500;
+    color: #ff7700;
+    margin-right: 2rem;
+    &--filled {
+      color: #fff;
+      background: #ff7700;
+    }
+  }
+  &__span {
+    color: #ff7700;
+  }
+}
+
+img {
+  height: 53.4rem;
+  width: 47rem;
+  border-radius: 0 20% 0 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div id="home-wrapper">
     <AppHeader></AppHeader>
+    <InfoSection
+      title="Take The Worry Out of Life with"
+      span="Insurance Protection"
+      text="Quisque libero justo, iaculis at justo sed, eleifend dictum odio. Duis
+        pharetra justo id justo rutrum, sed egestas urna sagittis. Nunc tempor
+        posuere ultricies. Nullam aliquet fringilla pulvinar. Donec sit amet
+        augue id tortor interdum facilisis vitae porta odio. Fusce congue porta
+        tempus. Nulla nec interdum eros, vitae pretium ante. Curabitur accumsan
+        velit purus."
+      btnOne="Get Started"
+      btnTwo="Find an agent"
+    ></InfoSection>
   </div>
 </template>
 
 <script>
 import AppHeader from "../components/AppHeader";
+import InfoSection from "../components/InfoSection";
 
 export default {
   name: "Home",
   components: {
     AppHeader,
+    InfoSection,
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#home-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
